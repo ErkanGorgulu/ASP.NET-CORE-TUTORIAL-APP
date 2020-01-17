a351420d2ce97ce20e2e82b3b2552c42e4624af7
ErkanGorgulu <erkangorgulu@gmail.com>
2020-01-17 19:20:28 +0300

    views are added

--- WEB-APP/Controllers/HomeController.cs
+++ WEB-APP/Controllers/HomeController.cs
@@ -9,16 +9,22 @@ namespace WEB_APP.Controller
 {
     public class HomeController: Microsoft.AspNetCore.Mvc.Controller
     {
-        private IEmployeeRepository _employeeRepository;
+        private readonly IEmployeeRepository _employeeRepository;
 
         public HomeController(IEmployeeRepository employeeRepository)
         {
             _employeeRepository = employeeRepository;
         }
-        public string Index()
+        public string Index(int id=1)
         {
 
-            return _employeeRepository.GetEmployee(1).Name;
+            return _employeeRepository.GetEmployee(Id:id).Name;
+        }
+        public ViewResult Details(int id=1)
+        {
+
+            Employee employee = _employeeRepository.GetEmployee(Id:id);
+            return View(employee);
         }
     }
 }
