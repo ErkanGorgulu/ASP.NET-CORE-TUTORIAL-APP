eb8e5c2873482128b568db5e6ba6d5f90cc6be96
ErkanGorgulu <erkangorgulu@gmail.com>
2020-01-17 17:34:46 +0300

    return json value from controller

--- WEB-APP/Controller/HomeController.cs
+++ WEB-APP/Controller/HomeController.cs
@@ -2,14 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 
 namespace WEB_APP.Controller
 {
-    public class HomeController
+    public class HomeController: Microsoft.AspNetCore.Mvc.Controller
     {
-        public string Index()
+        public JsonResult Index()
         {
-            return "Hello from home controller";
+            
+            return Json(new {id = 1, name = "Erkan"});
         }
     }
 }
